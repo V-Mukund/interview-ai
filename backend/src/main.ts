877a9dc0f7503c3465f448d6a0a3eca1bcdf5968
V-Mukund <mukundvijayakumar902@gmail.com>
2026-05-22 10:35:22 +0530

    fix cors allow frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,12 +14,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://welcoming-alignment-production-92b0.up.railway.app',
-      process.env.FRONTEND_URL,
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
