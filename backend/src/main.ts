b0e23f77f37e15cd4c1845640e41b98af2876c76
V-Mukund <mukundvijayakumar902@gmail.com>
2026-05-22 08:19:57 +0530

    fixed frontend api url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,25 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+//   await app.listen(8000);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(8000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 8000, '0.0.0.0');
+
+  console.log(`Backend running on port ${process.env.PORT || 8000}`);
 }
 bootstrap();
